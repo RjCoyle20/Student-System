c4c8eb956e5e940f511a20af0a7d5d0d9c457610
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-06-11 20:59:20 -0500

    Controller, service and implementaion set up. Database tested working via Postman.

--- src/main/java/com/devryan/studentsystem/service/StudentService.java
+++ src/main/java/com/devryan/studentsystem/service/StudentService.java
@@ -1,4 +1,8 @@
 package com.devryan.studentsystem.service;
 
+import com.devryan.studentsystem.model.Student;
+
 public interface StudentService {
+
+    public Student saveStudent(Student student);
 }
--- src/main/java/com/devryan/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/devryan/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,19 @@
 package com.devryan.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.devryan.studentsystem.model.Student;
+import com.devryan.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+
+        return studentRepository.save(student);
+    }
 }
